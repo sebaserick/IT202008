ac832f9f55ddd23b486af0d744ca2b61af9a0e53
sebaserick <60156182+sebaserick@users.noreply.github.com>
2020-01-31 20:58:50 -0500

    Create config.sample.php

---
+++ DBSetup/config.sample.php
@@ -0,0 +1,7 @@
+<?php
+
+$dbuser = "em288";//your ucid
+$dbpass = "VEcinito12?";//your phpMyAdmin password
+$dbhost = "sql1.njit.edu";//whichever server you login to from web.njit.edu/mysql/phpMyAdmin
+$dbdatabase = "em288";//your ucid
+?>
